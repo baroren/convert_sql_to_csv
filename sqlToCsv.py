f97c4466b10eac4cab1206ba4509b2447bb5e88b
Bar Oren <43580896+baroren@users.noreply.github.com>
2022-06-22 13:16:02 +0300

    Update and rename ex2_ranelbe_baror.py to sqlToCsv.py

--- sqlToCsv.py
+++ sqlToCsv.py
@@ -7,8 +7,7 @@ def convert_sql_to_csv(file_name):
         values_list = []
         if "CREATE TABLE" in line:
             fields = extract_fields(sql_file, line)
-            print (fields)
-            print("end of table\n")
+           
     sql_file.seek(0,0)
     for line in sql_file:
         if "INSERT INTO" in line:
@@ -16,10 +15,8 @@ def convert_sql_to_csv(file_name):
             values_list[0] = line.split("(")[1]
             values_list[-1] = values_list[-1][:-3]
             values = "".join(values_list)
-            print(values)
-            print("end of values\n")
-
-
+            
+            
 def extract_fields(sql_file, line):
     line = next(sql_file)
     first_word = line.split()[0]
@@ -31,7 +28,6 @@ def extract_fields(sql_file, line):
         first_word = line.split()[0]
     fields = ','.join(field_list)
     return fields
-
 
 def is_field(first_word):
     return first_word[0] == first_word[-1] == '`'
